6127fb77ff0666be70328b703d4601198c785e21
Maeda <shige.shige.0903114@gmail.com>
2016-05-19 18:35:12 +0900

    20160519

--- 02/contact.php
+++ 02/contact.php
@@ -1,28 +1,47 @@
+<link rel=stylesheet type="text/css" href="style.css">  
 <form action="result.php" method="post">
-<CENTER>
+
+<title>お問い合わせ</title>
+
+<center>
+<font size="+2"><b>お問い合わせ</b></font><br>
+<br>
+<br>
 <div>
-姓:<label><for="name1"></label>
-<input type="text" id="name1" size="20">
+姓　：<input type="text" name="name1" size="20"><br>
+名　：<input type="text" name="name2" size="20"><br>
 </div>
-<div>
-名:<label><for="name2"></label>
-<input type="text" id="name2" size="20">
-<div>
-電話番号:<input size="5" type="text" name="電話番号[][-]" /> - <input size="7" type="text" name="電話番号[][-]" /> - <input size="7" type="text" name="電話番号[][]" />
-</div>
-<div>
+
+    性別 ：<input type="radio" name="性別" value="男" /> 男　
+    <input type="radio" name="性別" value="女" /> 女
+    <input type="radio" name="性別" value="不明" />不明<br>
+
+    住所 ：<input type="text" name="Address" size="50"><br>
+
+    電話番号 ：<input size="5" type="text" name="number1"> - <input size="5" type="text" name="number2"> - <input size="5" type="text" name="number3"><br>
+
+    メールアドレス<input size="20" type="text" name="mail">@<input size="15" type="text" name="domain"><br>
+
+    <td><font>どこで知ったか</font></td><br>
+    <td><input type="radio" value="雑誌・広告" name="point">雑誌・広告<br>
+        <input type="radio" value="ネット" name="point">ネット<br>
+        <input type="radio" value="友人・家族" name="point">友人・家族<br>
+        <input type="radio" value="その他" name="point">その他</font></td><br>
+
     <p>
-質問カテゴリ:<select name="example1">  //プルダウン形式で表示
-    <option value="サンプル1">課題について</option>
-    <option value="サンプル2">日報の書き方</option>
-    <option value="サンプル3">メールの書き方</option>
-    <option value="サンプル4">趣味</option>
-    <option value="サンプル5">最近熱いアニメ</option>
+    質問カテゴリ ：<select name="question">
+    <option value="課題について">課題について</option>
+    <option value="日報の書き方">日報の書き方</option>
+    <option value="メールの書き方">メールの書き方</option>
+    <option value="趣味">趣味</option>
+    <option value="最近熱いアニメ">最近熱いアニメ</option>
     </select>
     </p>
-</div>
 
-<input type="submit">
+    <td>質問内容</td>
+    <textarea name="feelings" rows="5" cols="20"></textarea><br>
 
-</CENTER>
+
+<input type="submit" value="送信">　　　<input type="reset" value="リセット">
+</center>
 </form>
--- 02/result.php
+++ 02/result.php
@@ -1,6 +1,14 @@
 <?php
-echo $_POST['name1'];
-echo $_POST['name2'];
+echo $_POST["name1"] .  " " . $_POST["name2"]."<br>";
+echo $_POST["性別"]."<br>";
+echo $_POST["Address"]."<br>";
+echo $_POST["number1"] ."-" . $_POST["number2"]. "-". $_POST["number3"]."<br>";
+echo $_POST["mail"] . "@" . $_POST["domain"]."<br>";
+echo $_POST["point"]."<br>";
+echo $_POST["question"]."<br>";
+echo $_POST["feelings"]."<br>";
+
+
 
 
 ?>
