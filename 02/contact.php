5ca44ba1a4313d516a55f8c34fe6ed22f3b6e2e5
Maeda <shige.shige.0903114@gmail.com>
2016-05-20 13:54:15 +0900

    質問内容　テキストエリア固定

--- 02/contact.php
+++ 02/contact.php
@@ -58,8 +58,11 @@
     </tr>
     <tr>
     <th>質問内容　：</th>
+
         <td><textarea name="feelings" rows="5" cols="20"></textarea></td>
+    
     </tr>
+    </table>
 </table>
     <p align="center">
         <input type="submit" value="　　送信　　">　　　<input type="reset" value="リセット">
